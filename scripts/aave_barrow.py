a56df73e51d06e80c28025eb06b0c63c1e04ae74
juliocslima <lima.julio.cs@gmail.com>
2021-11-02 11:23:59 -0300

    Get borrowable data function

--- scripts/aave_barrow.py
+++ scripts/aave_barrow.py
@@ -17,6 +17,32 @@ Unit testes: Mainnet-fork (Mock all of mainnet!)
 amount = Web3.toWei(0.1, "ether")
 
 
+def main():
+    account = get_account()
+    erc20_address = config["networks"][network.show_active()]["weth_token"]
+    if network.show_active() in ["mainnet-fork"]:
+        get_weth()
+    lending_pool = get_lending_pool()
+    # Approve sending out ERC20 tokens
+    approve_erc20(amount, lending_pool.address, erc20_address, account)
+    print("Depositing...")
+    tx = lending_pool.deposit(erc20_address, amount,
+                              account.address, 0, {"from": account})
+    tx.wait(1)
+    print("deposited!")
+    # ... how much???
+    borrowable_eth, total_debt = get_borrowable_data(lending_pool, account)
+
+
+def approve_erc20(amount, spender, erc20_address, account):
+    print("Approving ERC20 token ...")
+    erc20 = interface.IERC20(erc20_address)
+    tx = erc20.approve(spender, amount, {"from": account})
+    tx.wait(1)
+    print("Approved!")
+    return tx()
+
+
 def get_lending_pool():
 
     lending_pool_addresses_provider = interface.ILendingPoolAddressesProvider(
@@ -37,16 +63,41 @@ def approve_erc20(amount, spender, erc20_address, account):
     return tx
 
 
-def main():
-    account = get_account()
-    erc20_address = config["networks"][network.show_active()]["weth_token"]
-    if network.show_active() in ["mainnet-fork"]:
-        get_weth()
-    lending_pool = get_lending_pool()
-    # Approve sending out ERC20 tokens
-    approve_erc20(amount, lending_pool.address, erc20_address, account)
-    print("Depositing...")
-    tx = lending_pool.deposit(erc20_address, amount,
-                              account.address, 0, {"from": account})
+def get_lending_pool():
+
+    lending_pool_addresses_provider = interface.ILendingPoolAddressesProvider(
+        config["networks"][network.show_active(
+        )]["lending_pool_addresses_provider"]
+    )
+    lending_pool_address = lending_pool_addresses_provider.getLendingPool()
+    lending_pool = interface.ILendingPool(lending_pool_address)
+    return lending_pool
+
+
+def approve_erc20(amount, spender, erc20_address, account):
+    print("Approving ERC20 token ...")
+    erc20 = interface.IERC20(erc20_address)
+    tx = erc20.approve(spender, amount, {"from": account})
     tx.wait(1)
-    print("deposited!")
+    print("Approved!")
+    return tx
+
+
+def get_borrowable_data(lending_pool, account):
+    (
+        total_collateral_eth,
+        total_debt_eth,
+        available_borrow_eth,
+        current_liquidation_thresold,
+        ltv,
+        health_factor,
+    ) = lending_pool.getUserAccountData(account.address)
+    available_borrow_eth = Web3.fromWei(available_borrow_eth, "ether")
+    total_collateral_eth = Web3.fromWei(total_collateral_eth, "ether")
+    total_debt_eth = Web3.fromWei(total_debt_eth, "ether")
+
+    print(f"You have {available_borrow_eth} worth of ETH deposited.")
+    print(f"You have {total_collateral_eth} worth of ETH borrowable.")
+    print(f"You can borrow {total_debt_eth} worth of ETH.")
+
+    return (float(available_borrow_eth), float(total_debt_eth))
