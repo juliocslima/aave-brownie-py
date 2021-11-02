1f1b00cb5c9a3d4bfa1b599d58f5b8e992dabff0
juliocslima <lima.julio.cs@gmail.com>
2021-11-01 22:37:28 -0300

    Get WETH function created

---
+++ scripts/aave_barrow.py
@@ -0,0 +1,10 @@
+from scripts.helpful_scripts import get_account
+from scripts.get_weth import get_weth
+from brownie import config, network
+
+
+def main():
+    account = get_account()
+    erc20_address = config["networks"][network.show_active()]["weth_token"]
+    if network.show_active() in ["mainnet-fork"]:
+        get_weth()
